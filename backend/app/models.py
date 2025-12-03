be30db63d6006d3f8d11a810d1e62b50051358f1
Orion Hess <orionhess@gmail.com>
2025-12-03 13:09:06 -0800

    feat: database structure and initial routes

---
+++ backend/app/models.py
@@ -0,0 +1,127 @@
+from database import db
+from datetime import datetime
+
+"""
+Objects:
+    user(user_id, username, email)
+    device(user_id, name) 
+        uid -> user(name) cascade
+    budget(budget_id, budget_name, user_id)
+        user_id -> user(user_id)
+    category(category_id, category_name, time_allocated, budget_id, group_id)
+        budget_id -> budget(budget_id)
+        group_id -> group(group_id) - nullable
+    group(group_id, group_name, budget_id)
+        budget_id -> budget(budget_id)
+    transaction(transaction_id, transaction_name, period, date_time, from?, category_id)
+        category_id -> category(category_id)
+    
+    notification()
+
+Relations:
+    authorizes(authorizer, authorized)
+        authorizer -> user(user_id)
+        authorized -> user(user_id)
+"""
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    user_id  = db.Column(db.Integer,     primary_key=True)
+    username = db.Column(db.String(80),  nullable=False)
+    email    = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime,  nullable=False, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'username': self.username,
+            'email': self.email,
+            'created_at': self.created_at.isoformat(),
+        }
+
+class device(db.Model):
+    __tablename__ = 'device'
+
+    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, cascade=True)
+    device_name = db.Column(db.String(80), primary_key=True)
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'device_name': self.device_name,
+        }
+
+class budget(db.Model):
+    __tablename__ = 'budget'
+
+    budget_id = db.Column(db.Integer, primary_key=True)
+    budget_name = db.Column(db.String(80), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'budget_id': self.budget_id,
+            'budget_name': self.budget_name,
+            'user_id': self.user_id,
+        }
+
+class category(db.Model):
+    __tablename__ = 'category'
+
+    category_id    = db.Column(db.Integer, primary_key=True)
+    category_name  = db.Column(db.String(80), nullable=False)
+    time_allocated = db.Column(db.DateTime, nullable=False)
+    budget_id      = db.Column(db.Integer, db.ForeignKey('budget.budget_id'), nullable=False)
+    group_id       = db.Column(db.Integer, db.ForeignKey('group.group_id'), nullable=True)
+
+    def to_dict(self):
+        return {
+            'category_id': self.category_id,
+            'category_name': self.category_name,
+            'time_allocated': self.time_allocated,
+            'budget_id': self.budget_id,
+            'group_id': self.group_id,
+        }
+
+class group(db.Model):
+    __tablename__ = 'group'
+
+    group_id = db.Column(db.Integer, primary_key=True)
+    group_name = db.Column(db.String(80), nullable=False)
+    budget_id = db.Column(db.Integer, db.ForeignKey('budget.budget_id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'group_id': self.group_id,
+            'group_name': self.group_name,
+            'budget_id': self.budget_id,
+        }
+
+class transaction(db.Model):
+    __tablename__ = 'transaction'
+
+    transaction_id   = db.Column(db.Integer, primary_key=True)
+    transaction_name = db.Column(db.String(80), nullable=False)
+    period           = db.Column(db.Interval, nullable=False)
+    date_time        = db.Column(db.DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            'transaction_id': self.transaction_id,
+            'transaction_name': self.transaction_name,
+            'period': self.period,
+            'date_time': self.date_time,
+        }
+
+class authorizes(db.Model):
+    __tablename__ = 'authorizes'
+
+    authorizer_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True, nullable=False)
+    authorized_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True, nullable=False)
+
+    def to_dict(self):
+        return {
+            'authorizer_id': self.authorizer_id,
+            'authorized_id': self.authorized_id,
+        }
---
+++ backend/app/routes/user.py
@@ -0,0 +1,29 @@
+from app.models import User
+from flask import Blueprint, jsonify, request
+
+user_bp = Blueprint('user', __name__, url_prefix='/api/user')
+
+@user_bp.route('', methods=['GET'])
+def get_users():
+    """Get all users"""
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users]), 200
+
+@user_bp.route('/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    """Get a single user"""
+    user = User.query.get(user_id)
+    return jsonify(user.to_dict()), 200
+
+@user_bp.route('', methods=['POST'])
+def create_user():
+    """Create a user"""
+    data = request.get_json()
+
+    if not data or not data.get("username") or not data.get("email"):
+        return jsonify({'error': 'Username and email are required'}), 400
+
+    user = User(
+        username=data.get("username"),
+        email=data.get("email"),
+    )
